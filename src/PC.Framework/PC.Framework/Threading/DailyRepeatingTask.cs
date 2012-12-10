1dfc9c7bfa0ae60214eb9749853cb23c1a796292
Tom <toms.squires@pebblecode.com>
2012-12-10 14:52:43 +0000

    Include PC.Framework

---
+++ src/PC.Framework/PC.Framework/Threading/DailyRepeatingTask.cs
@@ -0,0 +1,20 @@
+﻿using PebbleCode.Framework.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PebbleCode.Framework.Threading
+{
+    /// <summary>
+    /// A self starting, 24 hour period task
+    /// </summary>
+    public abstract class DailyRepeatingTask : RepeatingTask
+    {
+        public DailyRepeatingTask(TimeSpan timeToRun, ILogger logger)
+            : base(TimeSpan.FromHours(24), logger)
+        {
+            Start(timeToRun);
+        }
+    }
+}
