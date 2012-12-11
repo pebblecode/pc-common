bbbce4646cc78092ca22532943e38a509d4b558b
Tom <toms.squires@pebblecode.com>
2012-12-11 13:52:53 +0000

    Add PC.FSharp

---
+++ src/PC.FSharp/PC.ExpressionCalculator/AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿namespace PebbleCode.ExpressionCalculator
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.0.0.0")>]
+[<assembly: AssemblyFileVersion("0.0.0.0")>]
+do()
