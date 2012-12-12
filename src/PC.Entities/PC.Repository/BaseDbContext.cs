86d6b506a040b9544e80387508a4a72aa032bad0
Tom <toms.squires@pebblecode.com>
2012-12-12 09:14:12 +0000

    at repository to PC.entities

---
+++ src/PC.Entities/PC.Repository/BaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PebbleCode.Framework.IoC;
+
+namespace PebbleCode.Repository
+{
+    public abstract class BaseDbContext
+    {
+        public virtual RepositoryTransaction BeginTransaction()
+        {
+            return Kernel.Get<TransactionProvider>().BeginTransaction();
+        }
+    }
+}
